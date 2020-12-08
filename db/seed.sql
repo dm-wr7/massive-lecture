19f0593b2d03380b6d77c827e8b2eb202a2c7374
Andrew Westenskow <andrew.westenskow@gmail.com>
2020-12-08 10:01:05 -0700

    Initial commit

---
+++ db/seed.sql
@@ -0,0 +1,9 @@
+CREATE TABLE movies (
+  id SERIAL PRIMARY KEY,
+  title VARCHAR(200) NOT NULL,
+  poster TEXT,
+  year VARCHAR(4)
+);
+
+INSERT INTO movies (title, poster, year)
+VALUES ('Tenet', 'https://i.pinimg.com/originals/71/2b/46/712b465c00e58971bd8ce7becf7f8b46.png', 2020), ('Jurrasic Park', 'https://images-na.ssl-images-amazon.com/images/I/61RLRZnbc%2BL._AC_SL1000_.jpg', 1993);
